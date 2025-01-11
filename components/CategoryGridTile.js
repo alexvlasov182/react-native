495dd60e058bdd625d67f0d227d2697495312964
Oleksandr Vlasov <drumlife182@gmail.com>
2025-01-11 21:12:04 +0100

    GategoryGridTitle commit

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,6 +1,7 @@
 import { Pressable, View } from "react-native-web";
 
 function CategoryGridTile({ title, color }) {
+  console.log("CategoryGridTile", title, color);
   return (
     <View>
       <Pressable>
