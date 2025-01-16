aa7a6bae5824ad9df088ab297cdfa712409c84ba
Oleksandr Vlasov <drumlife182@gmail.com>
2025-01-16 23:49:40 +0100

    Genve commit

--- react-native-navigation/App.js
+++ react-native-navigation/App.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
+  console.log("App");
   return <CategoriesScreen />;
 }
 
