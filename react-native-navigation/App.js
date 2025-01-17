4b87971a597ebc49e4b49a189dcc6c46c817d3bb
Oleksandr Vlasov <drumlife182@gmail.com>
2025-01-17 21:02:40 +0100

    CERN

--- react-native-navigation/App.js
+++ react-native-navigation/App.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  console.log("App");
+  console.log("CERN");
   return <CategoriesScreen />;
 }
 
