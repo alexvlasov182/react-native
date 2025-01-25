a8968281dc6f83e660bcdd690080a20446d183db
Oleksandr Vlasov <drumlife182@gmail.com>
2025-01-25 10:52:37 +0100

    Added boxshadow to the card

--- react-native-navigation/components/CategoryGridTitle.js
+++ react-native-navigation/components/CategoryGridTitle.js
@@ -23,6 +23,12 @@ const styles = StyleSheet.create({
     margin: 16,
     height: 150,
     borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
   },
   button: {
     flex: 1,
