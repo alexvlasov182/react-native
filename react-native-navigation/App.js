215addd7dc2de78fb1af17d451cd8566cec43083
Oleksandr Vlasov <drumlife182@gmail.com>
2025-01-27 08:16:29 +0100

    Displaying Items in a Grid

--- react-native-navigation/App.js
+++ react-native-navigation/App.js
@@ -3,7 +3,14 @@ import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
--- react-native-navigation/components/CategoryGridTitle.js
+++ react-native-navigation/components/CategoryGridTitle.js
@@ -1,13 +1,19 @@
-import { Pressable, View, Text, StyleSheet } from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTitle({ title, color }) {
   console.log("CategoryGridTitle", title, color);
 
   return (
     <View style={styles.gridItem}>
-      <Pressable style={styles.button}>
-        <View style={styles.innerContainer}>
-          <Text>{title}</Text>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -23,19 +29,28 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 8,
     elevation: 4,
-    backgroundColor: "yellow",
+    backgroundColor: "white",
     shadowColor: "black",
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
   },
   button: {
     flex: 1,
   },
+  buttonPressed: {
+    opacity: 0.5,
+  },
   innerContainer: {
     flex: 1,
-    padding: 16,
     justifyContent: "center",
     alignItems: "center",
+    padding: 16,
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
