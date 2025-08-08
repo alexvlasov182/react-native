28cb6389b0abf73533ef20fdd259c6d84800b954
Oleksandr Vlasov <drumlife182@gmail.com>
2025-08-08 11:37:58 +0200

    docs: addied header buttons

--- react-native-navigation/App.js
+++ react-native-navigation/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -22,7 +23,11 @@ export default function App() {
               contentStyle: { backgroundColor: '#3f2f25' }
             }} />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen}  />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{
+            headerRight: () => {
+              return <Button title="Tap me!" />
+            }
+          }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
